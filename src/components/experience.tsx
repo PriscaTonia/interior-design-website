28859eff88c014d49805193f15318b7ac9b25064
Prisca Tonia <priscaebube@gmail.com>
2023-09-28 19:58:15 +0100

    netlify build 2

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -16,13 +16,14 @@ const Experience = ({
       } border-primary-100 items-center justify-center gap-4`}
     >
       <h5 className="text-primary-100 text-[50px] lg:text-[85px] tracking-widest leading-[70px] font-dm lg:leading-[106.25px] ">
-        <CountUp
+        {/* <CountUp
           duration={3}
           start={0}
           end={num}
           enableScrollSpy={true}
           scrollSpyDelay={0}
-        />
+        /> */}
+        {num}
       </h5>
       <p className="text-text-gray-200 text-base text-center lg:text-lg font-jost tracking-tight lg:leading-9">
         {text}
