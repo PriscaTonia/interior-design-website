0a06fc3ec191ee8b474e2b984fc0b7bd3a8eac67
Prisca Tonia <priscaebube@gmail.com>
2023-09-28 20:03:09 +0100

    Vercel Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // import vercelServerless from "@astrojs/vercel/serverless";
-
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
